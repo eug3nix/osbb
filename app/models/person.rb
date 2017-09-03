c2836462a372e787e2dc495295b6c47db2e4b340
Eugene Dubinin <eugend@commandprompt.com>
2017-09-03 21:04:44 +0300

    fixes crash on absend person.person_type field

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,10 +1,7 @@
 class Person < ActiveRecord::Base
-  belongs_to :person_type
   has_many :person_estates, dependent: :restrict_with_error
   has_many :estates, through: :person_estates
   has_many :contacts
-
-  validates :person_type, presence: true
 
   accepts_nested_attributes_for :estates, allow_destroy: true, reject_if: :all_blank
   accepts_nested_attributes_for :person_estates, allow_destroy: true, reject_if: :all_blank
